d9e9440700d1f913305442e6cc8817c67d92572b
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-07-08 20:10:57 -0500

    Add files via upload

---
+++ Windows-Communication-Foundation/TransportChannels/JsonRpcOverTcp/ConsoleTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JsonRpcOverTcp.Channels.Test;
+
+namespace JsonRpcOverTcp.ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = new RequestChannelTests();
+            tests.AsynchronousOpen();
+        }
+    }
+}
